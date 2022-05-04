5b123030279ebfc09eca6e869a1a992270b8dd98
André Ciornavei <andre.ciornavei@gmail.com>
2022-05-04 18:37:47 -0300

    controller normalization bug fix; all routes is disabled by default; routes can only be enabled if specified on routes.json

--- middlewares/route-permission/index.js
+++ middlewares/route-permission/index.js
@@ -17,53 +17,86 @@ module.exports = strapi => {
     },
     async initialize() {
 
+      // define roles on the first to be reusable
+      // by script and functions
+      const roles = {}
+
+      // define the permissions who will
+      // be enabled by default if it nos
+      // specified on routes
+      const DEFAULT_ENABLED_PERMISSIONS = [
+        // "public.users-permissions.auth.callback",
+        // "public.users-permissions.auth.connect",
+        // "public.users-permissions.auth.emailconfirmation",
+        // "public.users-permissions.auth.forgotpassword",
+        // "public.users-permissions.auth.forgotpassword",
+        // "public.users-permissions.auth.register",
+        // "public.users-permissions.auth.resetpassword",
+        // "public.users-permissions.auth.sendemailconfirmation",
+        // "authenticated.users-permissions.user.me",
+      ]
+
+      const configureRole = async (target_role) => {
+        // check if role already was registered on 'roles' variable
+        if (Object.keys(roles).includes(target_role)) return
+        // check if role exists on database
+        const role = await strapi.query('role', 'users-permissions').findOne({ type: target_role })
+        // create the role if it does not exists
+        if (!role) {
+          console.log("generating new role::", target_role)
+          await strapi.query("role", "users-permissions").create({
+            name: target_role.charAt(0).toUpperCase() + target_role.slice(1),
+            description: `Default role given to ${target_role} user.`,
+            type: target_role
+          })
+        }
+      }
+
+      const manageRole = async (type, plugin, controller, action) => {
+        const path = `${type}.${plugin}.${controller}`
+        _.set(roles, path, [...(_.get(roles, path, [])), String(action).toLowerCase()])
+      }
+
+
       try {
         // Update all permissions to enabled = false
-        await strapi.query('permission', 'users-permissions').model.where('type', 'application').save({
-          enabled: 0
-        }, {
-          method: "update",
-          patch: true
-        })
-      } catch (error) { console.log("No 'application' permissions to update...") }
+        await strapi.query('permission', 'users-permissions').model
+          .where("id", "!=", 0)
+          .save({ enabled: 0 }, { method: "update", patch: true })
+      } catch (error) { console.log("No permissions to update...") }
+
+      try {
+        // Update specified default permissions enabled = true
+        for (const permission of DEFAULT_ENABLED_PERMISSIONS) {
+          const [type, plugin, controller, action] = permission.split(".")
+          await configureRole(type)
+          await manageRole(type, plugin, controller, action)
+        }
+      } catch (error) { console.log("No default enabled permissions to update") }
+
 
       // Build route authentication role based on route 'config.permission' attribute
       // if it does not contains 'config.permission' assume Forbidden Access
-      const roles = {}
       const routes = _.get(strapi, "config.routes", []).map(route => injectPluginName(route, "application"))
       _.forEach(_.get(strapi, "plugins", []), (plugin, pluginName) => {
         const pluginRoutes = _.get(plugin, "config.routes", []).map(route => injectPluginName(route, pluginName))
         routes.push(...pluginRoutes)
       })
 
-      // _.forEach(routes, route => {
       for (const route of routes) {
         const [controller, action] = _.get(route, "handler").split(".")
         if (_.get(route, 'config.roles') || _.get(route, 'config.permission')) {
-          // treat for permission target to multi or single roles 
+          // treat for permission target to multi or single roles
           let target_roles = []
           const config_roles = _.get(route, "config.roles", _.get(route, 'config.permission'))
-          if (_.isString(config_roles)) target_roles = [config_roles]
-          else if (_.isArray(config_roles)) target_roles = config_roles
+          if (_.isString(config_roles)) target_roles = [config_roles.toLowerCase()]
+          else if (_.isArray(config_roles)) target_roles = config_roles.map(i => i.toLowerCase())
           // attatch permission to target roles
           for (const target_role of target_roles) {
-            // check if role already was registered on 'roles' variable
-            if (!Object.keys(roles).includes(target_role)) {
-              // check if role exists on database
-              const role = await strapi.query('role', 'users-permissions').findOne({ type: target_role })
-              // create the role if it does not exists
-              if (!role) {
-                console.log("generating new role::", target_role)
-                await strapi.query("role", "users-permissions").create({
-                  name: target_role,
-                  description: `Default role given to ${target_role} user.`,
-                  type: target_role
-                })
-              }
-            }
+            // configure role
+            await configureRole(target_role)
             // create path for controller permission and attach to roles
-            const path = `${target_role}.${_.get(route, "plugin")}.${controller}`
-            _.set(roles, path, [...(_.get(roles, path, [])), String(action).toLowerCase()])
+            await manageRole(target_role, _.get(route, "plugin"), controller, action)
           }
         }
       }
@@ -79,20 +112,21 @@ module.exports = strapi => {
             // re-create role for each plugin
             for (const controller in roles[role.type][plugin]) {
               for (const action of roles[role.type][plugin][controller]) {
+                const controllerLowerCase = String(controller).toLowerCase()
                 const actionLowerCase = String(action).toLowerCase()
                 const payloadId = {
                   type: plugin,
-                  controller: controller,
+                  controller: controllerLowerCase,
                   action: actionLowerCase,
                   role: role.id
                 }
                 const permission = await strapi.query('permission', 'users-permissions').findOne(payloadId)
                 if (permission) {
                   await strapi.query('permission', 'users-permissions').update({ id: permission.id }, { enabled: true })
-                  console.log(`updating permission ::: ${role.type}.application.${controller}.${actionLowerCase} ::: ID=${permission.id}`)
+                  console.log(`updating permission ::: ${role.type}.${plugin}.${controllerLowerCase}.${actionLowerCase} ::: ID=${permission.id}`)
                 } else {
                   const pId = await strapi.query('permission', 'users-permissions').create({ ...payloadId, enabled: true })
-                  console.log(`generating permission ::: ${role.type}.application.${controller}.${actionLowerCase} ::: ID=${pId.id}`)
+                  console.log(`generating permission ::: ${role.type}.${plugin}.${controllerLowerCase}.${actionLowerCase} ::: ID=${pId.id}`)
                 }
               }
             }
